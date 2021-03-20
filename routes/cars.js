01d995fa48ffc147dd667c2cd28f35dbdfca2d0e
Abdisamad Hussein <abdisamadh91@gmail.com>
2021-03-20 14:51:24 -0400

    added functioning form that appended to the database

--- routes/cars.js
+++ routes/cars.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const carsCtrl = require('../controllers/cars');
 
 router.get('/', carsCtrl.index);
-router.get('/:id', carsCtrl.show);
 router.get('/new', carsCtrl.new);
 router.post('/', carsCtrl.create);
+router.get('/:id', carsCtrl.show);
 
 module.exports = router;
