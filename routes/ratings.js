2b1d39fe6bdf5f85aea59d37064c0a93dd79931f
Abdisamad Hussein <abdisamadh91@gmail.com>
2021-03-23 20:55:54 -0400

    removed rasting route and controller

--- routes/ratings.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const ratingsCtrl = require('../controllers/ratings');
-
-router.post('/cars/:id/ratings', ratingsCtrl.create)
-
-
-module.exports = router;
