573e0b940b923ce588eef7172fa20cca1e1ce7b7
Abdisamad Hussein <abdisamadh91@gmail.com>
2021-03-25 20:01:41 -0400

    update removed and unused dependencies

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const port = process.env.PORT || 3000;
 const authorization = require('./utils/authorization');
-const favicon = require('serve-favicon')
-const path = require('path');
+
 
 require('./config/database');
 //require config file
@@ -19,7 +18,7 @@ const usersRouter = require('./routes/users');
 const app = express();
 
 //app middleware
-app.use(favicon(path.join(__dirname, 'public', 'imgs', 'car-solid.svg')));
+
 app.set('view engine', 'ejs');
 app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
