7186afec7316649f026a3d5ddc439fc1727a24d2
abdi-hu <78183395+abdi-hu@users.noreply.github.com>
2021-05-21 21:11:18 -0400

    removed oauth key

--- config/passport.js
+++ config/passport.js
@@ -5,10 +5,9 @@ const User = require("../models/user");
 passport.use(
 	new GoogleStrategy(
 		{
-			clientID:
-				"235976971756-5l6s6qvckqcftjr5bqjr49javarr1uh9.apps.googleusercontent.com",
-			clientSecret: "T39nU4sXvGP7US5vsDavqoyA",
-			callbackURL: "http://localhost:3000",
+			clientID: process.env.GOOGLE_CLIENT_ID,
+			clientSecret: process.env.GOOGLE_SECRET,
+			callbackURL: process.env.GOOGLE_CALLBACK,
 		},
 		function (accessToken, refreshToken, profile, cb) {
 			// a user has logged in with OAuth...
